2be6c600a37834504e316a7eec251f6781552824
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-03-15 14:44:08 -0600

    Add generalInfo state and input handler functions to change it

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,13 +1,51 @@
+import { useState } from 'react';
 import GeneralInfo from './GeneralInfo';
-import './Content.css';
 import Education from './Education';
+import Curriculum from './Curriculum';
+import './Content.css';
 
 function Content() {
+  const initialGeneral = {
+    name: null,
+    email: null,
+    phone: null,
+    address: null,
+  };
+  const [general, setGeneral] = useState(initialGeneral);
+
+  function handleNameChange(e) {
+    setGeneral({ ...general, name: e.target.value });
+  }
+
+  function handleEmailChange(e) {
+    setGeneral({ ...general, email: e.target.value });
+  }
+
+  function handlePhoneChange(e) {
+    setGeneral({ ...general, phone: e.target.value });
+  }
+
+  function handleAddressChange(e) {
+    setGeneral({ ...general, address: e.target.value });
+  }
+
   return (
-    <div className="editables">
-      <GeneralInfo />
-      <Education />
-    </div>
+    <>
+      <section className="editables">
+        <form action="">
+          <GeneralInfo
+            handleNameChange={handleNameChange}
+            handleEmailChange={handleEmailChange}
+            handlePhoneChange={handlePhoneChange}
+            handleAddressChange={handleAddressChange}
+          />
+          <Education />
+        </form>
+      </section>
+      <section className="displayable">
+        <Curriculum general={general} />
+      </section>
+    </>
   );
 }
 
--- src/GeneralInfo.jsx
+++ src/GeneralInfo.jsx
@@ -1,39 +1,58 @@
-function GeneralInfo() {
+function GeneralInfo({
+  handleNameChange,
+  handleEmailChange,
+  handlePhoneChange,
+  handleAddressChange,
+}) {
   return (
-    <section className="general-info">
-      <fieldset>
-        <h1>General Information</h1>
-        <label htmlFor="name">
-          <div>
-            <span>Full name</span>
-          </div>
-          <input
-            type="text"
-            id="name"
-            placeholder="First and last name"
-            required
-          />
-        </label>
-        <label htmlFor="email">
-          <div>
-            <span>Email</span> <span>recommended</span>
-          </div>
-          <input type="email" id="email" placeholder="ex. email@email.com" />
-        </label>
-        <label htmlFor="phone">
-          <div>
-            <span>Phone number</span> <span>recommended</span>
-          </div>
-          <input type="tel" id="phone" placeholder="10 digits phone number" />
-        </label>
-        <label htmlFor="address">
-          <div>
-            <span>Address</span> <span>recommended</span>
-          </div>
-          <input type="text" id="address" placeholder="City, Country" />
-        </label>
-      </fieldset>
-    </section>
+    <fieldset className="general-info">
+      <h1>General Information</h1>
+      <label htmlFor="name">
+        <div>
+          <span>Full name</span>
+        </div>
+        <input
+          type="text"
+          id="name"
+          placeholder="First and last name"
+          required
+          onChange={handleNameChange}
+        />
+      </label>
+      <label htmlFor="email">
+        <div>
+          <span>Email</span> <span>recommended</span>
+        </div>
+        <input
+          type="email"
+          id="email"
+          placeholder="ex. email@email.com"
+          onChange={handleEmailChange}
+        />
+      </label>
+      <label htmlFor="phone">
+        <div>
+          <span>Phone number</span> <span>recommended</span>
+        </div>
+        <input
+          type="tel"
+          id="phone"
+          placeholder="10 digits phone number"
+          onChange={handlePhoneChange}
+        />
+      </label>
+      <label htmlFor="address">
+        <div>
+          <span>Address</span> <span>recommended</span>
+        </div>
+        <input
+          type="text"
+          id="address"
+          placeholder="City, Country"
+          onChange={handleAddressChange}
+        />
+      </label>
+    </fieldset>
   );
 }
 
