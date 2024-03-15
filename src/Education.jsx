8029f0f1be6518575e6a6dd6dc9e540aef6ab982
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-03-14 22:34:49 -0600

    Add Education editable content

---
+++ src/Education.jsx
@@ -0,0 +1,15 @@
+import Study from './Study';
+import './Education.css';
+
+function Education() {
+  return (
+    <section className="education">
+      <fieldset>
+        <h1>Education</h1>
+        <Study />
+      </fieldset>
+    </section>
+  );
+}
+
+export default Education;
