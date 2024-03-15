cf482613803b5f2c9ef5a6d17b55a19df1307d36
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-03-14 22:47:29 -0600

    Wrap editables in div

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,11 +1,13 @@
 import GeneralInfo from './GeneralInfo';
 import './Content.css';
+import Education from './Education';
 
 function Content() {
   return (
-    <>
+    <div className="editables">
       <GeneralInfo />
-    </>
+      <Education />
+    </div>
   );
 }
 
