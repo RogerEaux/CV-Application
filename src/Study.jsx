f9c7a82c30f2d0edf49b7c3356183a8865507d12
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-03-16 00:42:57 -0600

    Control Study inputs by adding onChange and value

--- src/Study.jsx
+++ src/Study.jsx
@@ -1,37 +1,65 @@
-function Study() {
+function Study({ study, handlers }) {
   return (
     <>
-      <label>
+      <label htmlFor="school">
         <div>
           <span>School</span>
         </div>
-        <input type="text" placeholder="School of study" />
+        <input
+          type="text"
+          placeholder="School of study"
+          id="school"
+          onChange={handlers.handleSchoolChange}
+          value={study.school}
+        />
       </label>
-      <label>
+      <label htmlFor="degree">
         <div>
           <span>Degree/Title</span>
         </div>
-        <input type="text" placeholder="Degree/Title of study" />
+        <input
+          type="text"
+          placeholder="Degree/Title of study"
+          id="degree"
+          onChange={handlers.handleDegreeChange}
+          value={study.degree}
+        />
       </label>
       <div className="date">
-        <label>
+        <label htmlFor="date-from">
           <div>
             <span>From</span>
           </div>
-          <input type="date" />
+          <input
+            type="date"
+            id="date-from"
+            onChange={handlers.handleDateFromChange}
+            value={study.dateFrom}
+          />
         </label>
-        <label>
+        <label htmlFor="date-to">
           <div>
             <span>To </span>
           </div>
-          <input type="date" />
+          <input
+            type="date"
+            id="date-to"
+            onChange={handlers.handleDateToChange}
+            value={study.dateTo}
+          />
         </label>
       </div>
-      <label>
+      <label htmlFor="study-location">
         <div>
           <span>Location</span> <span>optional</span>
         </div>
-        <input type="text" placeholder="Location of study" />
+        <input
+          type="text"
+          placeholder="Location of study"
+          id="study-location"
+          onChange={handlers.handleLocationChange}
+          value={study.location}
+        />
       </label>
     </>
   );
