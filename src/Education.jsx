338612181a3b9830c4f86b883f2f67ee97e24769
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-03-16 00:42:09 -0600

    Condition render to display education items or Study edit

--- src/Education.jsx
+++ src/Education.jsx
@@ -1,11 +1,26 @@
 import Study from './Study';
 import './Education.css';
 
-function Education() {
+function Education({ education, editedID, handlers }) {
   return (
     <fieldset>
       <h1>Education</h1>
-      <Study />
+      {editedID ? (
+        <Study
+          study={education.find((ed) => ed.id === editedID)}
+          handlers={handlers}
+        />
+      ) : (
+        <ul>
+          {education.map((study) => {
+            return (
+              <li key={study.id} onClick={handlers.handleEditStudy}>
+                <p data-id={study.id}>{study.school}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </fieldset>
   );
 }
