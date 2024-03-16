16288e661a13e0359b3c869c258e696052f199f7
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-03-16 00:41:00 -0600

    Add Education state and handlers

--- src/Content.jsx
+++ src/Content.jsx
@@ -6,12 +6,25 @@ import './Content.css';
 
 function Content() {
   const initialGeneral = {
-    name: null,
-    email: null,
-    phone: null,
-    address: null,
+    name: 'Juan',
+    email: 'juan@juan.com',
+    phone: '33 44 55 66 77',
+    address: 'Juan, Mexico',
   };
+  const initialEducation = [
+    {
+      id: 1,
+      school: 'University of Cheese',
+      degree: 'Cheesology',
+      dateFrom: '2020-03-15',
+      dateTo: '2024-03-15',
+      location: 'CheeseLand',
+    },
+  ];
   const [general, setGeneral] = useState(initialGeneral);
+  // const [educationID, setEducationID] = useState(2);
+  const [education, setEducation] = useState(initialEducation);
+  const [educationEditedID, setEducationEditedID] = useState(null);
 
   function handleNameChange(e) {
     setGeneral({ ...general, name: e.target.value });
@@ -29,21 +42,89 @@ function Content() {
     setGeneral({ ...general, address: e.target.value });
   }
 
+  function handleEditStudy(e) {
+    setEducationEditedID(parseInt(e.target.dataset.id));
+  }
+
+  function handleSchoolChange(e) {
+    setEducation([
+      ...education.filter((study) => !study.id === educationEditedID),
+      {
+        ...education.find((study) => study.id === educationEditedID),
+        school: e.target.value,
+      },
+    ]);
+  }
+
+  function handleDegreeChange(e) {
+    setEducation([
+      ...education.filter((study) => !study.id === educationEditedID),
+      {
+        ...education.find((study) => study.id === educationEditedID),
+        degree: e.target.value,
+      },
+    ]);
+  }
+
+  function handleDateFromChange(e) {
+    setEducation([
+      ...education.filter((study) => !study.id === educationEditedID),
+      {
+        ...education.find((study) => study.id === educationEditedID),
+        dateFrom: e.target.value,
+      },
+    ]);
+  }
+
+  function handleDateToChange(e) {
+    setEducation([
+      ...education.filter((study) => !study.id === educationEditedID),
+      {
+        ...education.find((study) => study.id === educationEditedID),
+        dateTo: e.target.value,
+      },
+    ]);
+  }
+
+  function handleStudyLocationChange(e) {
+    setEducation([
+      ...education.filter((study) => !study.id === educationEditedID),
+      {
+        ...education.find((study) => study.id === educationEditedID),
+        location: e.target.value,
+      },
+    ]);
+  }
+
   return (
     <>
       <section className="editables">
         <form action="">
           <GeneralInfo
-            handleNameChange={handleNameChange}
-            handleEmailChange={handleEmailChange}
-            handlePhoneChange={handlePhoneChange}
-            handleAddressChange={handleAddressChange}
+            general={general}
+            handlers={{
+              handleNameChange: handleNameChange,
+              handleEmailChange: handleEmailChange,
+              handlePhoneChange: handlePhoneChange,
+              handleAddressChange: handleAddressChange,
+            }}
           />
-          <Education />
+          <Education
+            education={education}
+            editedID={educationEditedID}
+            handlers={{
+              handleEditStudy: handleEditStudy,
+              handleSchoolChange: handleSchoolChange,
+              handleDegreeChange: handleDegreeChange,
+              handleDateFromChange: handleDateFromChange,
+              handleDateToChange: handleDateToChange,
+              handleLocationChange: handleStudyLocationChange,
+            }}
+          />
         </form>
       </section>
       <section className="displayable">
-        <Curriculum general={general} />
+        <Curriculum general={general} education={education} />
       </section>
     </>
   );
